3659779138ada3b0c4e8267535325979e2595b2e
YeQiu <YeQiu@example.com>
2026-01-14 19:28:28 -0600

    Java Practice

--- src/MatchCards.java
+++ src/MatchCards.java
@@ -33,7 +33,46 @@ public class MatchCards {
     };
 
 
+    int rows = 4;
+    int columns = 5;
+    int cardWidth = 90;
+    int cardHeight = 128;
+
+    ArrayList<Card> cardSet;
+    ImageIcon cardBackImageIcon;
+
+    int boardWidth = columns * cardWidth; // 5 * 128
+
     MatchCards() {
-        System.out.println("Match");
+        setupCards();
+        shuffleCards();
+    }
+
+    void setupCards() {
+        cardSet = new ArrayList<Card>();
+        for(String cardName : cardList) {
+            Image cardImg = new ImageIcon(getClass().getResource("./img/" + cardName + ".jpg")).getImage();
+            ImageIcon cardImageIcon = new ImageIcon(cardImg.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
+
+            Card card = new Card(cardName, cardImageIcon);
+            cardSet.add(card);
+        }
+
+        cardSet.addAll(cardSet);
+
+        Image cardBackImage = new ImageIcon(getClass().getResource("./img/back.jpg")).getImage();
+        cardBackImageIcon = new ImageIcon(cardBackImage.getScaledInstance(cardWidth, cardHeight, Image.SCALE_SMOOTH));
+    }
+
+    void shuffleCards() {
+        System.out.println(cardSet);
+        //shuffle
+        for (int i = 0; i < cardSet.size(); i++){
+            int j = (int) (Math.random() * cardSet.size());
+            Card temp = cardSet.get(i);
+            cardSet.set(i, cardSet.get(j));
+            cardSet.set(j, temp);
+
+        }
     }
 }
